4082afc0d46568276286302203375ef2aa1d8980
Himanshu vaishy <vaishhimanshu83170@gmail.com>
2026-01-09 14:03:14 +0530

    props

---
+++ vite-project/src/Props.jsx
@@ -0,0 +1,9 @@
+
+
+const Props=({user})=>{
+    const {name,age}=user;
+    
+    return <div>Hello, {name}!</div>;
+}
+
+export default Props;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Props from './props.jsx'
 
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    // <App />
+    <Props user={{ name:"Himanshu" ,age:20}}/>
  
 )
